f098561637af1c08d6f7e008597f49b447cde0e7
rohzart <rohzart@gmail.com>
2025-08-18 01:45:30 +0530

    new page to display the report as html and another to download as CSV

--- pages/status_report.php
+++ pages/status_report.php
@@ -2,21 +2,49 @@
 
 require_once(dirname( __FILE__, 2 ) . DIRECTORY_SEPARATOR . 'core/status_report_api.php');
 
+access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
+layout_page_header( lang_get( 'StatusReport_plugin_title' ) );
+layout_page_begin( 'status_report.php' );
+
 $report = get_monthly_time_report();
+?>
 
-$t_separator = ',';
+<div class="col-md-12 col-xs-12">
+	<h1>Status Report</h1>
 
-if (!$report) {
-	echo "No records found.";
-	return;
-} else {
-	$body .= "Project".$t_separator."Hours".$t_separator."Cost(USD) \n";
-	foreach ($report as $entry) {
-		$body .= $entry['project_name'].$t_separator.$entry['total_hours'].$t_separator.$entry['cost']."\n";
-	}
-	$filename = "status_report_".date("Ymd_Hi").".csv";
-	header( 'Content-Disposition: attachment; filename="'.$filename.'"' );
-	echo $body;
-}
-// header( 'Content-Disposition: attachment; filename="no_results.txt"' );
-// echo $content;
+	<a href="<?php echo plugin_page( 'status_report_csv.php' ) ?>" class="btn btn-primary btn-sm btn-white btn-round">Download CSV</a>
+
+	<div class="table-responsive">
+		<table class="table table-bordered table-condensed table-striped">
+		<thead>
+			<tr>
+				<th>Project</th>
+				<th>Start Date</th>
+				<th>End Date</th>
+				<th>Hours</th>
+				<th>Hourly Rate</th>
+				<th>Cost(USD)</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach ($report as $entry) {
+		?>
+			<tr>
+				<td><?php echo htmlspecialchars($entry['project_name']); ?></td>
+				<td><?php echo htmlspecialchars($entry['start_date']); ?></td>
+				<td><?php echo htmlspecialchars($entry['end_date']); ?></td>
+				<td><?php echo htmlspecialchars($entry['total_hours']); ?></td>
+				<td><?php echo htmlspecialchars($entry['hourly_rate']); ?></td>
+				<td><?php echo htmlspecialchars($entry['cost']); ?></td>
+			</tr>
+		<?php
+		}
+		?>
+		</tbody>
+		</table>
+	</div>
+</div>
+
+<?php
+layout_page_end();
---
+++ pages/status_report_csv.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(dirname( __FILE__, 2 ) . DIRECTORY_SEPARATOR . 'core/status_report_api.php');
+
+$report = get_monthly_time_report();
+
+$t_separator = ',';
+
+if (!$report) {
+	echo "No records found.";
+	return;
+} else {
+	$body .= "Project".$t_separator."Hours".$t_separator."Cost(USD) \n";
+	foreach ($report as $entry) {
+		$body .= $entry['project_name'].$t_separator.$entry['total_hours'].$t_separator.$entry['cost']."\n";
+	}
+	$filename = "status_report_".date("Ymd_Hi").".csv";
+	header( 'Content-Disposition: attachment; filename="'.$filename.'"' );
+	echo $body;
+}
+// header( 'Content-Disposition: attachment; filename="no_results.txt"' );
+// echo $content;
